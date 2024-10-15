b744684a704e5c486af958f955aa9c193ffb4727
Omonjon2004 <inatovomonjon8@gmail.com>
2024-10-15 09:09:22 +0500

    added Account

---
+++ apps/basket/api_endpoints/basket_item/views.py
@@ -0,0 +1,16 @@
+from rest_framework import viewsets
+
+from apps.basket.api_endpoints.basket_item.serializers import BasketItemSerializer
+from apps.basket.models import BasketItem
+
+
+class BasketItemViewSet(viewsets.ModelViewSet):
+    queryset = BasketItem.objects.all()
+    serializer_class = BasketItemSerializer
+
+    def get_queryset(self):
+        # Agar basket id'siga qarab filterlashni istasangiz
+        basket_id = self.kwargs.get('basket_id')
+        if basket_id:
+            return self.queryset.filter(basket_id=basket_id)
+        return super().get_queryset()
